6ffa8f42ecde0764f843485387e0b66a34801981
abhaydee <abhaygirish1997@gmail.com>
2022-12-03 14:48:04 -0500

    initial project setup

---
+++ src/aed_final_project_vaccination/AED_Final_Project_Vaccination.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aed_final_project_vaccination;
+
+/**
+ *
+ * @author abhaydeshpande
+ */
+public class AED_Final_Project_Vaccination {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
